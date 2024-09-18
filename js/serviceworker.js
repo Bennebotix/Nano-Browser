6792873480fa9d1471290ee52c82c04eefc91872
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-09-17 20:38:39 -0500

    Update and rename registerSW.js to serviceworker.js

---
+++ js/serviceworker.js
@@ -0,0 +1,13 @@
+const sw = new NanoServiceWorker();
+
+async function handleRequest(event) {
+    if (sw.route(event)) {
+        return await sw.fetch(event);
+    }
+    
+    return await fetch(event.request)
+}
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event));
+});
